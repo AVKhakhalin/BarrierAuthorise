37d87d7b3f2ed6f38a7e36e2a80f7a23b8ca8907
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-09-05 21:32:32 +0300

    Добавлен класс NetwokrStatus для определения состояния сети.

--- app/src/main/java/com/github/oauth/repositories/barrierauthorise/di/KoinModules.kt
+++ app/src/main/java/com/github/oauth/repositories/barrierauthorise/di/KoinModules.kt
@@ -3,9 +3,11 @@ package com.github.oauth.repositories.barrierauthorise.di
 import com.github.oauth.repositories.barrierauthorise.navigator.AppScreens
 import com.github.oauth.repositories.barrierauthorise.navigator.AppScreensImpl
 import com.github.oauth.repositories.barrierauthorise.utils.CICERONE_NAME
+import com.github.oauth.repositories.barrierauthorise.utils.network.NetworkStatus
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Router
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
@@ -16,6 +18,10 @@ val application = module {
         get<Cicerone<Router>>(named(CICERONE_NAME)).getNavigatorHolder() }
     single<Router> { get<Cicerone<Router>>(named(CICERONE_NAME)).router }
     single<AppScreens> { AppScreensImpl() }
+
+    // Вспомогательные классы:
+    // Определение статуса сети
+    single<NetworkStatus> { NetworkStatus(androidContext()) }
 }
 
 val screens = module {
